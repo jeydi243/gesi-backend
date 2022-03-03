e92cca800aaa2606c6eebb4c4124c7353f3db260
jeydi243 <ilungakadiongo@gmail.com>
2022-03-03 13:45:20 +0200

    remove unused imports

--- src/teachers/dto/create-teacher.dto.ts
+++ src/teachers/dto/create-teacher.dto.ts
@@ -1,6 +1,3 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, isPhoneNumber, ValidateIf } from 'class-validator';
-import { Name } from 'src/export.type';
 import { BaseMemberDto } from '../../member.base';
 
 export class CreateTeacherDto extends BaseMemberDto {}
