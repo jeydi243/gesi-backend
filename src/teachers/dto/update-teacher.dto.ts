f0202aec9318800281ac66f9efa8415bf885bbc4
jeydi243 <ilungakadiongo@gmail.com>
2023-06-29 10:08:58 +0200

    just delete teacher as module

--- src/teachers/dto/update-teacher.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTeacherDTO } from './create-teacher.dto';
-
-export class UpdateTeacherDTO extends PartialType(CreateTeacherDTO) {}
