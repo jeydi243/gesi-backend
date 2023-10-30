1c197043a7cbc6d6f0b1cf5b4334f7c1ad777f52
KADIONGO ILUNGA Epaphrodite <kadiongo.ilunga@gecamines.cd>
2023-10-30 11:41:49 +0200

    update storage.ts

--- src/resource/storage.ts
+++ src/resource/storage.ts
@@ -11,7 +11,7 @@ export class GridFsMulterConfigService implements MulterOptionsFactory {
   constructor() {
     console.log('Initialise GridFsMulterConfigService ... %s %s', process.env.NODE_ENV,process.env.MONGO_URI_DEV);
     this.gridFsStorage = new GridFsStorage({
-      url: process.env.NODE_ENV == 'development' ? process.env.MONGO_URI_DEV : process.env.MONGO_URI_PROD,
+      url: process.env.NODE_ENV == 'development' ? process.env.MONGO_URI_DEV : process.env.MONGO_ATLAS_URI,
       file: function (req, file) {
         console.log("Cette fonction s'execute pour chaque fichier");
 
